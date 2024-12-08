fcc6a5637d2c14f839ac29532898cd4f13d77e18
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-12-08 15:39:43 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
 function hasCycle(graph) {
+    let value = Object.keys(graph);
+    if(value.length == 0) {
+        return false;
+    }
+    let prev = []
+    let cycles = false;
+    for (let i = 0; i < value.length; i++) {
+        prev[i] = false;
+    }
+    for(let j = 0; j < value.length; j++) {
+        if(!prev[j] && cycleSearch(graph, j, prev)) {
+            cycles = true;
+            break;
+        }
+    }
+    return cycles;
+}
+
+function cycleSearch(graph, start, visited) {
+    
+
     return false;
 }
